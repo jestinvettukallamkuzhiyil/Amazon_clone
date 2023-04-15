e8676a00f6f971b9be25564e79c0d662c06e2b18
jestin joseph <108120741+jestinvettukallamkuzhiyil@users.noreply.github.com>
2023-04-15 12:57:56 +0530

    2

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyDEGtEaub_llyzDq8xgQiRf-1x6pJ1jzGg",
+  authDomain: "clone-61d9b.firebaseapp.com",
+  projectId: "clone-61d9b",
+  storageBucket: "clone-61d9b.appspot.com",
+  messagingSenderId: "618845296644",
+  appId: "1:618845296644:web:cde4f9feb817e8c973bd51",
+  measurementId: "G-SFBBZJ6ZV1",
 };
 
 const app = initializeApp(firebaseConfig);
